c9ac196fae4dc9cf1421e79ed0a5d253afe853bb
dgrzyl <daniel.grzyl.it@gmail.com>
2023-06-14 12:26:48 +0200

    Changing file name

---
+++ US-POL_units_calc.py
@@ -0,0 +1,19 @@
+# US-Poland units calculator by dgrztl
+
+print("#"* 40)
+print("")
+print("WELCOME TO UNITS CALCULATOR BY DGRZYL")
+print("")
+print("#"* 40)
+print("")
+
+def printmainmenu():
+    print("Choose what units you want to calculate: (type 1-7)")
+    print("1) Celsius to Fahrenheit")
+    print("2) Liters to Gallons")
+    print("3) Meters to Feet")
+    print("4) Kilometers to Miles")
+    print("5) Kilograms to Pounds")
+    print("6) Grams to Ounces")
+    
+printmainmenu()
