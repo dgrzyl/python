14e689d0503ba32567a27ab15ef97d18c6e1f79e
dgrzyl <daniel.grzyl.it@gmail.com>
2023-06-16 10:14:56 +0200

    Adding products function implementation

--- foodpicker.py
+++ foodpicker.py
@@ -1,2 +1,17 @@
 # Foodpicker - program do podpowiadania co możesz zjeść na podstawie rzeczy w Twojej lodówce
 
+products_list = []
+
+def productAdd():
+    while True:
+        product = input("Podaj produkty z lodówki: (wpisz f aby zakończyć wprowadzanie produktów)").lower()
+        if product != "f":
+            products_list.append(product)
+        else:
+            break
+
+
+
+
+# productAdd()
+# print(products_list)
