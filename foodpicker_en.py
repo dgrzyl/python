b0ecbc185ebb41fde9e2555e3844735c775ba6a1
dgrzyl <daniel.grzyl.it@gmail.com>
2023-06-27 14:33:24 +0200

    Making foodpicker_en and updating foodpicker

--- foodpicker_en.py
+++ foodpicker_en.py
@@ -1,11 +1,11 @@
-# Foodpicker - program do podpowiadania co możesz zjeść na podstawie rzeczy w Twojej lodówce
+# Foodpicker - An app that suggests what you can eat based on the items in your fridge
 import time
 
 products_list = []
 
 def productAdd():
     while True:
-        product = input("Podaj produkty z lodówki(wpisz f aby zakończyć wprowadzanie produktów)>> ").lower()
+        product = input("Please provide the items from the fridge (type 'f' to finish entering the products)>> ").lower()
         if product != "f":
             products_list.append(product)
         elif product == "":
@@ -15,12 +15,12 @@ def productAdd():
 
 def readFood():
 
-    with open('meals.txt', 'r') as food:
+    with open('meals_en.txt', 'r') as food:
         meals = food.readlines()
         meals = [meal.strip() for meal in meals]
 
     
-    with open('ingredients.txt', 'r') as ing:
+    with open('ingredients_en.txt', 'r') as ing:
         ingredients = ing.readlines()
         ingredients = [ingredients.strip() for ingredients in ingredients]
         for i in range(len(ingredients)):
@@ -39,16 +39,16 @@ def foodCompare(meals, ingredients, products_list):
 def main():
     print('#' * 85)
     print('')
-    print('Witaj w programie foodpicker! Ten program podpowie Ci co możesz zrobić do jedzenia!')
+    print('Welcome to the Foodpicker program! This program will suggest what you can make for a meal!')
     print('')
     print('#' * 85)
     time.sleep(2)
-    print('INFO: Wpisuj produkty pojedyńczo! Nie używaj polskich znaków oraz przecinków! Wielkość liter dowolna. Przykładowo wpisz: jajka lub oliwa z oliwek i naciśnij enter')
+    print('INFO: Enter the products one by one! Do not use Polish characters or commas! Letter case doesn\'t matter. For example, enter: eggs or olive oil and press enter')
     time.sleep(2)
     productAdd()
     meals, ingredients = readFood()
     foodCompare(meals, ingredients, products_list)
-    print("Potrawy, które możesz zrobić to:")
+    print("The meals you can make are:")
     time.sleep(1)
     for i in prop:
         print("-", i)
